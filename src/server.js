1cd436a6af467c4c1c03f5bdde823b2071736ab3
Hoàng Đức <hoangevil512@gmail.com>
2019-10-06 22:50:02 +0700

    [features/f21] Adapt changes from watch-manager (#4)

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ require('dotenv-flow').config()
 
 const fastify = require('fastify')
 const axios = require('axios')
+const { ObjectID } = require('mongodb')
 const { getCollection } = require('./database')
 
 const { NODE_ENV, PORT, SCHEDULER_ADDRESS } = process.env
@@ -36,6 +37,42 @@ server.post('/', async (req, res) => {
     })
 
     res.code(status)
+  } catch (err) {
+    req.log.error(err.message)
+    res.code(500)
+  }
+})
+
+server.get('/:id', async (req, res) => {
+  try {
+    const _id = ObjectID(req.params.id)
+    const watchCollection = await getCollection('watches')
+    const result = await watchCollection.findOne({ _id })
+    res.code(200).send(result)
+  } catch (err) {
+    req.log.error(err.message)
+    res.code(500)
+  }
+})
+
+server.put('/:id/targets', async (req, res) => {
+  try {
+    const _id = ObjectID(req.params.id)
+    const watchCollection = await getCollection('watches')
+    const updatedAt = new Date()
+    const watch = await watchCollection.findOne({ _id })
+    const { targets } = watch
+    for (const updateTarget of req.body) {
+      const { name } = updateTarget
+      for (let target of targets) {
+        if (name === target.name) {
+          target.data = updateTarget.data
+          target.updatedAt = updatedAt
+        }
+      }
+    }
+    watchCollection.updateOne({ _id }, { $set: { targets, updatedAt } })
+    res.code(204)
   } catch (err) {
     req.log.error(err.message)
     res.code(500)
